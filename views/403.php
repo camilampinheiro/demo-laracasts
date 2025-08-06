20a8fb3fce83640b27c5a02d46d36a032f5c1438
Camila Martins Pinheiro <camilamartins@aridesa.com.br>
2025-08-06 10:53:30 -0300

    botao de delete e autorizacao para deletar

--- views/403.php
+++ views/403.php
@@ -4,8 +4,8 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <h1 class="text-2xl font-bold">You are not authorized to view this page.</h1>
-    <p class="mt-4">
-      <a href="/" class="text-blue-500 underline">Go back home.</a>
+    <p class="mt-7">
+      <a href="/" class="rounded-md bg-[#1C242D] px-6 py-2 text-sm font-semibold text-white shadow-xs hover:bg-gray-700">Go back home</a>
     </p>
   </div>
 </main>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -4,10 +4,17 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <p class="mb-6 bg-[#1C242D] py-3 text-white mt-6 w-1/9 text-center rounded-md hover:bg-gray-700">
+    <!-- <p class="mb-6 bg-[#1C242D] py-3 text-white mt-6 w-1/9 text-center rounded-md hover:bg-gray-700">
       <a href="/notes">go back...</a>
-    </p>
-    <p><?= htmlspecialchars($note['body']); ?></p>
+    </p> -->
+    <p class="mt-6"><?= htmlspecialchars($note['body']); ?></p>
+
+    <form class="mt-6" method="POST">
+      <input type="hidden" name="id" value="<?=$note['id']; ?>">
+      <button class="bg-red-700 py-3 text-white mt-6 w-1/9 text-center rounded-md hover:bg-red-800 cursor-pointer">
+        Delete
+      </button>
+    </form>
   </div>
 </main>
 
