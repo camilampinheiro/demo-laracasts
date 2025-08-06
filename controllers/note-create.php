b2180fc5cd2c766ba8f83625f50374e957a12fc3
camilampinheiro <camilampinheiro1108@gmail.com>
2025-08-05 23:54:14 -0300

    começando o note create

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,6 +2,10 @@
 
 $heading = "Create Note";
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  dd('you submitted the form!');
+}
+
 require 'views/note-create.view.php';
 
 ?>
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -1,11 +1,29 @@
-<?php require ('views/partials/head.php'); ?>
-<?php require ('views/partials/nav.php'); ?>
-<?php require ('views/partials/banner.php'); ?>
+<?php require('views/partials/head.php'); ?>
+<?php require('views/partials/nav.php'); ?>
+<?php require('views/partials/banner.php'); ?>
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <h1>Create a note</h1>
+    <form method="POST">
+      <div class="space-y-12">
+        <div class="pb-12">
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="col-span-full">
+              <label for="about" class="block text-sm/6 font-medium text-gray-900">Note</label>
+              <div class="mt-2">
+                <textarea id="about" name="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="submit" class="rounded-md bg-[#1C242D] px-6 py-2 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2">Save</button>
+      </div>
+    </form>
+
   </div>
 </main>
 
-<?php require ('views/partials/footer.php'); ?>
+<?php require('views/partials/footer.php'); ?>
